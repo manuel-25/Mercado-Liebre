9a2b9903b92dbfe16d325b1ecfa6a3279cb2f987
Manuel Otamendi <73025455+manuel-25@users.noreply.github.com>
2021-12-09 20:03:53 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.set ('port', process.env.PORT || 3030)
+
+app.listen(app.get('port'), () => console.log('Servidor Corriendo'))
+
+app.get('/home', (req, res) => res.sendFile(path.resolve(__dirname,'./views/home.html')))
+app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname,'./views/register.html')))
+app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname,'./views/login.html')))
+
+app.use(express.static(path.resolve(__dirname, 'public')))
